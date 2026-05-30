c82c950316df20ab4e46bc69280a2bb621a48ffc
promise2000-oss <promisechuks93@gmail.com>
2026-05-30 08:26:26 +0100

    package.json: Updated the name field from doctarr to Doctarr

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,7 +5,15 @@ type IconProps = {
 
 export default function Icon({ icon, className = '' }: IconProps) {
   return (
-    <span className={`material-symbols-outlined ${className}`} data-icon={icon}>
+    <span
+      className={`material-symbols-outlined ${className}`}
+      data-icon={icon}
+      style={{
+        fontVariationSettings: "'FILL' 1",
+        fontSize: 'inherit',
+        fontWeight: 'inherit',
+      }}
+    >
       {icon}
     </span>
   )
